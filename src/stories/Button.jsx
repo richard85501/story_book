37c9cf2c521445fa6481282e38cac94ac8c145b1
richard85501 <liuchang85501@gmail.com>
2023-01-24 17:31:43 +0800

    Add button -type,icon,iconPlacen

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,50 +1,75 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({
+  backgroundColor,
+  size,
+  label,
+  type,
+  onClick,
+  icon,
+  iconPlace,
+  ...props
+}) => {
+  Button.propTypes = {
+    /**
+     * What background color to use
+     */
+    backgroundColor: PropTypes.string,
+    /**
+     * How large should the button be?
+     */
+    size: PropTypes.oneOf(["small", "medium", "large"]),
+    /**
+     * Button contents
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     * Optional click handler
+     */
+    onClick: PropTypes.func,
+    /**
+     * which type of button
+     */
+    type: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
+    /**
+     * where icon be
+     */
+    icon: PropTypes.node,
+    /**
+     * where icon be
+     */
+    iconPlace: PropTypes.oneOf(["before", "after"]),
+  };
+
+  Button.defaultProps = {
+    backgroundColor: null,
+    primary: false,
+    size: "medium",
+    onClick: undefined,
+    type: "primary",
+    icon: null,
+    iconPlace: icon ? "before" : null,
+  };
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        "storybook-button",
+        `storybook-button--${size}`,
+        `storybook-button--${type}`,
+      ].join(" ")}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
+      {iconPlace === "before" && icon}
       {label}
+      {iconPlace === "after" && icon}
     </button>
   );
-};
-
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
 };
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-
-import { Button } from './Button';
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { Button } from "./Button";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
@@ -14,23 +14,25 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  type: "primary",
+  label: "Button",
+  icon: <AiFillBug />,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  type: "secondary",
+  label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
